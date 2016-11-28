b077192cf7877079f6b0ffa4702f12e721c62ff9
Buka Cakrawala <kaka10garuda@gmail.com>
2016-11-28 11:09:50 -0800

    added Alamofire, SwiftyJSON and track down user location

--- PARKR/ViewController.swift
+++ PARKR/ViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 import MapKit
-class ViewController: UIViewController {
+import CoreLocation
 
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    let showAlert = UIAlertController()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        //When authorization status is not determined.
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else if CLLocationManager.authorizationStatus() == .denied {
+            showAlert.message = "Location services were previously denied. Please enable location services for this app in settings."
+        } else if CLLocationManager.authorizationStatus() == .authorizedAlways {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        //Triggers the location permission dialog.
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .followWithHeading
+        
       
     }
 
@@ -20,7 +50,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    //This function calls back when the information of the location comes back.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegionMake(location.coordinate, span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    //to check if there is an error
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
 }
 

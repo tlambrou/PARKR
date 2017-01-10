10f0ed30ccb20181b7b170ba366a0eb6c2286d52
Buka Cakrawala <kaka10garuda@gmail.com>
2017-01-09 17:25:35 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	PARKR/ViewController.swift

---
+++ PARKR/EnlargeMap.swift
@@ -0,0 +1,18 @@
+//
+//  EnlargeMap.swift
+//  PARKR
+//
+//  Created by Buka Cakrawala on 1/9/17.
+//  Copyright © 2017 SsosSoft. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class EnlargeMap: UIViewController {
+    
+    @IBOutlet weak var largerMapView: MKMapView!
+    
+    
+}
